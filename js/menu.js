5f25d28bfedd79530ed828fe5e67da535f4bc281
Maxim Kucheriaviy <maxim-k@i.ua>
2022-04-19 12:58:05 +0300

    fixes is done

--- js/menu.js
+++ js/menu.js
@@ -9,6 +9,7 @@
     refs.openModalBtn.addEventListener("click", toggleModal);
   
     function toggleModal() {
+      refs.openModalBtn.classList.toggle("header__menu-button--is-open")
       refs.modal.classList.toggle("header__menu--hidden");
       refs.svgLine.classList.toggle("is-hidden");
       refs.svgCrost.classList.toggle("is-hidden");
